63d222f5bc4a3c3bc869241ca28ef400b8b7bae1
GARY ROUSLTONE <gary.roulstone@repknight.com>
2022-10-03 14:44:31 +0100

    Definition of a Type

---
+++ types.ts
@@ -0,0 +1,15 @@
+//Stephen Grider Typescript: The Complete Develops Guide
+//Section 2: What is a Type System?
+
+const today = new Date();
+today.getMonth();
+
+const person = {
+  age: 20
+};
+
+class Color {
+  
+}
+
+const red = new Color();
